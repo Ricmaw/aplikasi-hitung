6142cf696489c44148f0455e0a9dd213ec11e775
Ricmaw <rickydarmawan040901@gmail.com>
2024-04-24 17:12:07 +0700

    penambahan hasil hitung

---
+++ segitiga.js
@@ -0,0 +1,11 @@
+console.log("berapa lima + 3 ?")
+const lima = 5
+const tiga = 7
+const liga = lima + tiga
+console.log(liga)
+
+console.log("hasilnya salah")
+console.log("yang benar adalah")
+// ini adalah hasil yang benar
+const liga_benar = lima + 3
+console.log(liga_benar)
